2bb151b374b0d476aa61949715fe7b591f7349c2
itsgarryb <garry@bhatt.al>
2019-04-25 15:39:50 +0200

    js refactoring

--- index.js
+++ index.js
@@ -1,17 +1,127 @@
-class TargetElement{constructor(topPosition,bottomPosition){this._topPosition=topPosition;this._bottomPostion=bottomPosition;this._onScreen=!1}
-get topPosition(){return this._topPosition}
-get bottomPosition(){return this._bottomPostion}
-get isOnScreen(){if(this.bottomPosition>=0&&this.topPosition<window.innerHeight){return!0}else{return!1}}
-set updatePosition(element){this._topPosition=element.getBoundingClientRect().top;this._bottomPostion=element.getBoundingClientRect().bottom}}
-class AnimateIt{constructor(animationName,startPosition){this._animationName=animationName;this._startPosition=startPosition;this._repetition=!0}
-get animationName(){return this._animationName}
-get startPosition(){return this._startPosition}
-get repetition(){return this._repetition}
-set setAnimationName(name){if(typeof name==='string'){this._animationName=name}else{this._animationName='none'}}
-set repetition(word){if(word==='single'){this._repetition=!1}else{this._repetition=!0}}
-set animationPosition(y){if(isNaN(y)||y>100||y<=0){this._startPosition=window.innerHeight*0.95}else{this._startPosition=window.innerHeight/100*y}}
-animateIt(element){element.style.visibility='visible';element.style.WebkitAnimationName=this.animationName;element.style.animationName=this.animationName}
-hideIt(element){element.style.visibility='hidden';element.style.WebkitAnimationName='none';element.style.animationName='none'}}
-const InitAi=()=>{const allDataAi=document.querySelectorAll('[data-ai]');Array.from(allDataAi).forEach((element)=>{const target=new TargetElement();const anim=new AnimateIt();const inputString=element.dataset.ai;const keyWords=inputString.split(" ");anim.setAnimationName=window.getComputedStyle(element).getPropertyValue('animation-name');anim.animationPosition=parseInt(keyWords[0]);anim.repetition=keyWords[1];target.updatePosition=element;if(target.isOnScreen){anim.animateIt(element)}else{anim.hideIt(element)}
-let lastCall=0;window.addEventListener('scroll',()=>{const now=(new Date()).getTime();if(now-lastCall<100){return}else{lastCall=now;target.updatePosition=element;if(target.isOnScreen&&target.topPosition<=anim.startPosition&&element.style.visibility==='hidden'){anim.animateIt(element)}else if(!target.isOnScreen&&anim.repetition){anim.hideIt(element)}}})})}
-InitAi()
+class TargetElement {
+
+    constructor(topPosition, bottomPosition){
+        this._topPosition = topPosition;
+        this._bottomPostion = bottomPosition; 
+        this._onScreen = false;
+    }
+
+    get topPosition() {
+        return this._topPosition;
+    }
+
+    get bottomPosition() {
+        return this._bottomPostion;
+    }
+
+    get isOnScreen() {
+        return (this.bottomPosition >= 0 && this.topPosition < window.innerHeight) || false;
+    }
+
+    set updatePosition (element) {
+        this._topPosition = element.getBoundingClientRect().top;
+        this._bottomPostion = element.getBoundingClientRect().bottom;
+    }
+}
+
+class AnimateIt {
+    constructor (animationName, startPosition) {
+        this._animationName = animationName;
+        this._startPosition = startPosition;
+        this._repetition = true;
+    }
+
+    get animationName() {
+        return this._animationName;
+    }
+
+    get startPosition() {
+        return this._startPosition;
+    }
+
+    get repetition() {
+        return this._repetition;
+    }
+
+    set setAnimationName (name) {
+        this._animationName = (typeof name === 'string') ? name : 'none';
+    }
+
+    set repetition(word) {
+        this._repetition = !((word === 'single') || false);
+    }
+
+    set animationPosition(y) {
+        this._startPosition = evaluatePosition(y);
+    }
+
+    evaluatePosition(y) {
+        switch(y) {
+            case (isNaN(y) || y > 100): 
+                return window.innerHeight * 0.97;
+            case y <= 0:
+                return window.innerHeight * 0.03;
+            default:
+                return window.innerHeight / 100 * y;
+        }
+    }
+
+    animateIt(element) {
+        element.style.visibility = 'visible';
+        element.style.WebkitAnimationName = this.animationName;
+        element.style.animationName = this.animationName;
+    }
+
+    hideIt(element) {
+        element.style.visibility = 'hidden';
+        element.style.WebkitAnimationName = 'none';
+        element.style.animationName = 'none';
+    }
+}
+
+class Helper {
+    static hideElement(target, anim) {
+        if(!target.isOnScreen && anim.repetition) {
+            anim.hideIt(element);
+        }
+    }
+
+    static isElementInsideOfScreen(target, element, anim) {
+        return target.isOnScreen &&
+        target.topPosition <= anim.startPosition &&
+        element.style.visibility === 'hidden';
+    }
+}
+
+const InitAi = () => {
+
+    const allDataAi = document.querySelectorAll('[data-ai]');
+
+    Array.from(allDataAi, (element) => {
+
+        const target = new TargetElement();
+        const anim = new AnimateIt();
+        const inputString = element.dataset.ai;
+        const keyWords = inputString.split(" ");
+
+        anim.setAnimationName = window.getComputedStyle(element).getPropertyValue('animation-name');
+        anim.animationPosition = parseInt(keyWords[0]);
+        anim.repetition = keyWords[1];
+        target.updatePosition = element;
+        (target.isOnScreen) ? anim.animateIt(element) : anim.hideIt(element);
+
+        let lastCall = 0;
+
+        window.addEventListener('scroll', () => {
+            const now = (new Date()).getTime();
+
+            if (now - lastCall > 100) {
+                lastCall = now;
+                target.updatePosition = element;
+                (Helper.isElementInsideOfScreen(target, element, anim)) ? anim.animateIt(element) : Helper.hideElement(target, anim);
+            }
+        });
+    });
+}
+
+InitAi();
